8cc86db8632b4c0225dc72cc3365de10fe92f961
Vijay Mocherla <vijaysai.mocherla@gmail.com>
2025-12-15 18:07:30 +0530

    added elements array with atomic numbers

--- src/constants.f90
+++ src/constants.f90
@@ -26,8 +26,14 @@ module constants
     use, intrinsic :: iso_fortran_env
 
     implicit none
-
     integer, parameter :: dp = REAL64
+    
+    type atomic_element
+        character(len=3) :: symbol
+        integer :: atomic_number
+        real(dp) :: mass
+    end type atomic_element
+    
 
     ! Physical and mathematical constants
     real(dp), parameter :: sqrt2          = sqrt(2.0_dp)
@@ -69,6 +75,7 @@ module constants
     integer, parameter :: pes_id_simple_lj      = 2006
     integer, parameter :: pes_id_no_interaction = 2007
     integer, parameter :: pes_id_nene           = 2008
+    integer, parameter :: pes_id_mace           = 2009
 
     ! PES names
     character(len=*), parameter :: pes_name_lj             = "lj"
@@ -79,6 +86,7 @@ module constants
     character(len=*), parameter :: pes_name_ho             = "ho"
     character(len=*), parameter :: pes_name_morse          = "morse"
     character(len=*), parameter :: pes_name_nene           = "nene"
+    character(len=*), parameter :: pes_name_mace           = "mace"
 
     ! Debug ID
     integer, parameter :: debug_id_lj                 = 1
@@ -168,5 +176,128 @@ module constants
     real(dp), parameter :: evang2habohr         = 1.0_dp/habohr2evang
     real(dp), parameter :: habohrcub2evangcub   = ha2ev*(ang2bohr**3)
     real(dp), parameter :: au2gpa               = 29419.844_dp ! 1 Ha/Bohr3 = 29419.844 GPa
+
+
+    ! Atomic elements
+    type(atomic_element), parameter :: elements(118) = [ &
+                atomic_element('H  ',   1,      1.007), & ! Hydrogen
+                atomic_element('He ',   2,      4.002), & ! Helium
+                atomic_element('Li ',   3,      6.941), & ! Lithium
+                atomic_element('Be ',   4,      9.012), & ! Beryllium
+                atomic_element('B  ',   5,      10.811), & ! Boron
+                atomic_element('C  ',   6,      12.011), & ! Carbon
+                atomic_element('N  ',   7,      14.007), & ! Nitrogen
+                atomic_element('O  ',   8,      15.999), & ! Oxygen
+                atomic_element('F  ',   9,      18.998), & ! Fluorine
+                atomic_element('Ne ',   10,     20.18), & ! Neon
+                atomic_element('Na ',   11,     22.99), & ! Sodium
+                atomic_element('Mg ',   12,     24.305), & ! Magnesium
+                atomic_element('Al ',   13,     26.982), & ! Aluminum
+                atomic_element('Si ',   14,     28.086), & ! Silicon
+                atomic_element('P  ',   15,     30.974), & ! Phosphorus
+                atomic_element('S  ',   16,     32.065), & ! Sulfur
+                atomic_element('Cl ',   17,     35.453), & ! Chlorine
+                atomic_element('Ar ',   18,     39.948), & ! Argon
+                atomic_element('K  ',   19,     39.098), & ! Potassium
+                atomic_element('Ca ',   20,     40.078), & ! Calcium
+                atomic_element('Sc ',   21,     44.956), & ! Scandium
+                atomic_element('Ti ',   22,     47.867), & ! Titanium
+                atomic_element('V  ',   23,     50.942), & ! Vanadium
+                atomic_element('Cr ',   24,     51.996), & ! Chromium
+                atomic_element('Mn ',   25,     54.938), & ! Manganese
+                atomic_element('Fe ',   26,     55.845), & ! Iron
+                atomic_element('Co ',   27,     58.933), & ! Cobalt
+                atomic_element('Ni ',   28,     58.693), & ! Nickel
+                atomic_element('Cu ',   29,     63.546), & ! Copper
+                atomic_element('Zn ',   30,     65.38), & ! Zinc
+                atomic_element('Ga ',   31,     69.723), & ! Gallium
+                atomic_element('Ge ',   32,     72.64), & ! Germanium
+                atomic_element('As ',   33,     74.922), & ! Arsenic
+                atomic_element('Se ',   34,     78.96), & ! Selenium
+                atomic_element('Br ',   35,     79.904), & ! Bromine
+                atomic_element('Kr ',   36,     83.798), & ! Krypton
+                atomic_element('Rb ',   37,     85.468), & ! Rubidium
+                atomic_element('Sr ',   38,     87.62), & ! Strontium
+                atomic_element('Y  ',   39,     88.906), & ! Yttrium
+                atomic_element('Zr ',   40,     91.224), & ! Zirconium
+                atomic_element('Nb ',   41,     92.906), & ! Niobium
+                atomic_element('Mo ',   42,     95.96), & ! Molybdenum
+                atomic_element('Tc ',   43,     9855), & ! Technetium
+                atomic_element('Ru ',   44,     101.07), & ! Ruthenium
+                atomic_element('Rh ',   45,     102.906), & ! Rhodium
+                atomic_element('Pd ',   46,     106.42), & ! Palladium
+                atomic_element('Ag ',   47,     107.868), & ! Silver
+                atomic_element('Cd ',   48,     112.411), & ! Cadmium
+                atomic_element('In ',   49,     114.818), & ! Indium
+                atomic_element('Sn ',   50,     118.71), & ! Tin
+                atomic_element('Sb ',   51,     121.76), & ! Antimony
+                atomic_element('Te ',   52,     127.6), & ! Tellurium
+                atomic_element('I  ',   53,     126.904), & ! Iodine
+                atomic_element('Xe ',   54,     131.293), & ! Xenon
+                atomic_element('Cs ',   55,     132.905), & ! Cesium
+                atomic_element('Ba ',   56,     137.327), & ! Barium
+                atomic_element('La ',   57,     138.905), & ! Lanthanum
+                atomic_element('Ce ',   58,     140.116), & ! Cerium
+                atomic_element('Pr ',   59,     140.908), & ! Praseodymium
+                atomic_element('Nd ',   60,     144.242), & ! Neodymium
+                atomic_element('Pm ',   61,     145.00), & ! Promethium
+                atomic_element('Sm ',   62,     150.36), & ! Samarium
+                atomic_element('Eu ',   63,     151.964), & ! Europium
+                atomic_element('Gd ',   64,     157.25), & ! Gadolinium
+                atomic_element('Tb ',   65,     158.925), & ! Terbium
+                atomic_element('Dy ',   66,     162.5), & ! Dysprosium
+                atomic_element('Ho ',   67,     164.93), & ! Holmium
+                atomic_element('Er ',   68,     167.259), & ! Erbium
+                atomic_element('Tm ',   69,     168.934), & ! Thulium
+                atomic_element('Yb ',   70,     173.054), & ! Ytterbium
+                atomic_element('Lu ',   71,     174.967), & ! Lutetium
+                atomic_element('Hf ',   72,     178.49), & ! Hafnium
+                atomic_element('Ta ',   73,     180.948), & ! Tantalum
+                atomic_element('W  ',   74,     183.84), & ! Wolfram
+                atomic_element('Re ',   75,     186.207), & ! Rhenium
+                atomic_element('Os ',   76,     190.23), & ! Osmium
+                atomic_element('Ir ',   77,     192.217), & ! Iridium
+                atomic_element('Pt ',   78,     195.084), & ! Platinum
+                atomic_element('Au ',   79,     196.967), & ! Gold
+                atomic_element('Hg ',   80,     200.59), & ! Mercury
+                atomic_element('Tl ',   81,     204.383), & ! Thallium
+                atomic_element('Pb ',   82,     207.20), & ! Lead
+                atomic_element('Bi ',   83,     208.98), & ! Bismuth
+                atomic_element('Po ',   84,     210.00), & ! Polonium
+                atomic_element('At ',   85,     210.00), & ! Astatine
+                atomic_element('Rn ',   86,     222.00), & ! Radon
+                atomic_element('Fr ',   87,     223.00), & ! Francium
+                atomic_element('Ra ',   88,     226.00), & ! Radium
+                atomic_element('Ac ',   89,     227.00), & ! Actinium
+                atomic_element('Th ',   90,     232.00), & ! Thorium
+                atomic_element('Pa ',   91,     231.00), & ! Protactinium
+                atomic_element('U  ',   92,     238.00), & ! Uranium
+                atomic_element('Np ',   93,     237.00), & ! Neptunium
+                atomic_element('Pu ',   94,     244.00), & ! Plutonium
+                atomic_element('Am ',   95,     243.00), & ! Americium
+                atomic_element('Cm ',   96,     247.00), & ! Curium
+                atomic_element('Bk ',   97,     247.00), & ! Berkelium
+                atomic_element('Cf ',   98,     251.00), & ! Californium
+                atomic_element('Es ',   99,     252.00), & ! Einsteinium
+                atomic_element('Fm ',   100,    257.00), & ! Fermium
+                atomic_element('Md ',   101,    258.00), & ! Mendelevium
+                atomic_element('No ',   102,    259.00), & ! Nobelium
+                atomic_element('Lr ',   103,    262.00), & ! Lawrencium
+                atomic_element('Rf ',   104,    261.00), & ! Rutherfordium
+                atomic_element('Db ',   105,    262.00), & ! Dubnium
+                atomic_element('Sg ',   106,    266.00), & ! Seaborgium
+                atomic_element('Bh ',   107,    264.00), & ! Bohrium
+                atomic_element('Hs ',   108,    267.00), & ! Hassium
+                atomic_element('Mt ',   109,    268.00), & ! Meitnerium
+                atomic_element('Ds ',   110,    271.00), & ! Darmstadtium
+                atomic_element('Rg ',   111,    272.00), & ! Roentgenium
+                atomic_element('Cn ',   112,    285.00), & ! Copernicium
+                atomic_element('Nh ',   113,    284.00), & ! Nihonium
+                atomic_element('Fl ',   114,    289.00), & ! Flerovium
+                atomic_element('Mc ',   115,    288.00), & ! Moscovium
+                atomic_element('Lv ',   116,    292.00), & ! Livermorium
+                atomic_element('Ts ',   117,    295.00), & ! Tennessine
+                atomic_element('Og ',   118,    294.00) & ! Oganesson
+]
 
 end module constants
